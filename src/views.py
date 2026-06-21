ed40a84be1d66ea5fd9b5496fa9463a562657b48
Your Name <you@example.com>
2026-06-21 18:06:35 +0300

    реализация функции greeting_by_time

--- src/views.py
+++ src/views.py
@@ -25,15 +25,15 @@ def read_datafile(filename: str = "operations.xlsx") -> List[Dict[Hashable, Any]
     Путь по умолчанию "operations.xlsx" (все файлы храняться в папке data)
     """
     file_path = os.path.abspath((os.path.join(root_path, "..", "data", filename)))
-    logger.info(f"вызов read_excel_file с путем до файла: {file_path}")
+    logger.info(f"вызов read_datafile с путем до файла: {file_path}")
     try:
         df = pd.read_excel(file_path)
         transactions = df.to_dict(orient="records")
-        logger.info("read_excel_file завершение функции")
+        logger.info("read_datafile завершение функции")
         return transactions
     except Exception as e:
         transactions = []
-        logger.error(f"read_excel_file завершение функции с ошибкой({e}). На выходе пустой список")
+        logger.error(f"read_datafile завершение функции с ошибкой({e}). На выходе пустой список")
         return transactions
 
 
@@ -42,6 +42,7 @@ def greeting_by_time() -> str:
     Фунция выводит приветствие в зависимости от текущего времени системы в формате строки
     Например: "Доброе утро" с 06:00 по 11:59
     """
+    logger.info("вызов greeting_by_time")
     greetings_time = [
         {"Доброе утро": ["06:00", "11:59"]},
         {"Добрый день": ["12:00", "17:59"]},
@@ -50,6 +51,7 @@ def greeting_by_time() -> str:
     ]
     result_greeting = ""
     current_time = datetime.datetime.now().time()
+    logger.info(f"greeting_by_time получение текущего времени: {current_time}")
     for greeting in greetings_time:
         for key, value in greeting.items():
             # по каждому диапазону сравниваем текущее время
@@ -60,4 +62,5 @@ def greeting_by_time() -> str:
             ):
                 result_greeting = key
                 break
+    logger.info(f"greeting_by_time завершение работы функции с параметром: {result_greeting}")
     return result_greeting
