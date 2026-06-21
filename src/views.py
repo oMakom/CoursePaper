6cab76acbd2c52151a8266ffd32061c12803a037
Your Name <you@example.com>
2026-06-21 14:41:21 +0300

    правки кода под PEP8

--- src/views.py
+++ src/views.py
@@ -1,15 +1,16 @@
-import os
-import json
-import requests
 import datetime
+import json
 import logging
-import pandas as pd
+import os
 from typing import Any, Dict, Hashable, List
+
+import pandas as pd
+import requests
 
 # Определяем путь к проекту Src
 root_path = os.path.dirname(os.path.abspath(__file__))
 # Определяем путь к файлу logs/views.log
-logs_path = os.path.abspath(os.path.join(root_path, '..', 'logs/views.log'))
+logs_path = os.path.abspath(os.path.join(root_path, "..", "logs/views.log"))
 logger = logging.getLogger("views")
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler(logs_path, mode="w", encoding="utf-8")
@@ -17,12 +18,13 @@ file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(m
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
-def read_datafile(filename: str = 'operations.xlsx') -> List[Dict[Hashable, Any]]:
+
+def read_datafile(filename: str = "operations.xlsx") -> List[Dict[Hashable, Any]]:
     """
     Функция читает exls файл. Принимает на фход путь к файлу и выводит список словарей (при ошибках пустой список)
     Путь по умолчанию "operations.xlsx" (все файлы храняться в папке data)
     """
-    file_path = os.path.abspath((os.path.join(root_path,'..', 'data', filename)))
+    file_path = os.path.abspath((os.path.join(root_path, "..", "data", filename)))
     logger.info(f"вызов read_excel_file с путем до файла: {file_path}")
     try:
         df = pd.read_excel(file_path)
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,10 @@
-from unittest.mock import patch, mock_open
+from unittest.mock import MagicMock, mock_open, patch
 
 from src.views import read_datafile
 
 
 @patch("builtins.open", side_effect=FileNotFoundError)
-def test_read_exls_file_not_found(mock_open):
+def test_read_exls_file_not_found(mock_open: MagicMock) -> None:
     """Тест обработки ошибки отсутствия файла."""
     result = read_datafile("nonexistent.exls")
     # вернула пустой список
@@ -14,7 +14,7 @@ def test_read_exls_file_not_found(mock_open):
 
 
 @patch("builtins.open", mock_open(read_data="некорректный exls {]"))
-def test_exls_decode_error():
+def test_exls_decode_error() -> None:
     """Тест обработки некорректного xlsx."""
     result = read_datafile("test_invalid.exls")
     assert result == []
